bc3a15203ae513a4f3b38caa4b1299cd19d01744
ga14a <ga14axy14@gmail.com>
2023-02-08 14:36:29 +0900

    1주차

---
+++ 주은하/Grade.java
@@ -0,0 +1,6 @@
+package 주은하;
+
+public class Grade {
+    String subject;
+    int score;
+}
---
+++ 주은하/GradeOutput.java
@@ -0,0 +1,55 @@
+package 주은하;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GradeOutput {
+    public static void main(String[] args) {
+        List<Grade> grades = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+
+        //성적 입력
+        while(true) {
+            System.out.println("과목명을 입력해 주세요.");
+            String mySub = sc.next();
+            System.out.println("성적을 입력해 주세요.");
+            int mySco = sc.nextInt();
+            Grade grade = new Grade();
+            grade.subject = mySub;
+            grade.score = mySco;
+            grades.add(grade);
+
+            System.out.println("성적을 산출하려면 0, 더 입력하려면 1을 입력하세요.");
+            int check = sc.nextInt();
+            if(check != 0 && check != 1) {
+                System.out.println("다시 입력하세요.");
+            }
+            if(check == 0) {
+                break;
+            }
+        }
+        //성적 계산
+        int sum = 0;
+        int avg;
+        for(Grade grade : grades) {
+            if(grade.score < 30) {
+                System.out.println(grade.subject + " : F");
+            }else if(grade.score < 60){
+                System.out.println(grade.subject + " : D");
+            }else if(grade.score < 80){
+                System.out.println(grade.subject + " : C");
+            }else if(grade.score < 95){
+                System.out.println(grade.subject + " : B");
+            }else{
+                System.out.println(grade.subject + " : A");
+            }
+            sum += grade.score;
+        }
+        avg = sum / grades.size();
+
+        System.out.println("총점 : " + sum);
+        System.out.println("평균 : " + avg);
+
+    }
+}
