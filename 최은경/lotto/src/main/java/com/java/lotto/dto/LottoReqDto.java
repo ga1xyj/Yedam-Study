7a9d858e97b64672183b0ed0cc21ed6c5216984a
dmskk <eek4416@gmail.com>
2023-02-12 22:45:43 +0900

    update: remove annotation-Nullable

--- 최은경/lotto/src/main/java/com/java/lotto/dto/LottoReqDto.java
+++ 최은경/lotto/src/main/java/com/java/lotto/dto/LottoReqDto.java
@@ -2,17 +2,14 @@ package com.java.lotto.dto;
 
 import com.java.lotto.entity.Lotto;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.lang.Nullable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class LottoReqDto {
 
-    @Nullable
     private int id;
     private String num;
 
